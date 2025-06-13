642abd7bc024d65b1650ce34632e07ed7d53ce13
adnatecodez <adeaduratimilehin@gmail.com>
2025-06-13 23:33:04 +0100

    lift off

--- src/main.tsx
+++ src/main.tsx
@@ -1,42 +1,12 @@
-import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
 
-// Import the generated route tree
-import { routeTree } from './routeTree.gen'
+const rootElement = document.getElementById('app')!
 
-import './styles.css'
-import reportWebVitals from './reportWebVitals.ts'
+import { RouterProvider } from '@tanstack/react-router'
+import { createRouter } from './router'
+const router = createRouter()
 
-// Create a new router instance
-const router = createRouter({
-  routeTree,
-  context: {},
-  defaultPreload: 'intent',
-  scrollRestoration: true,
-  defaultStructuralSharing: true,
-  defaultPreloadStaleTime: 0,
-})
-
-// Register the router instance for type safety
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
-}
-
-// Render the app
-const rootElement = document.getElementById('app')
-if (rootElement && !rootElement.innerHTML) {
+if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
-  root.render(
-    <StrictMode>
-      <RouterProvider router={router} />
-    </StrictMode>,
-  )
+  root.render(<RouterProvider router={router} />)
 }
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
